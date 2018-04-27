c9084f249c1e4430ee1f2f60fc0e5965448b8659
Rajkeshwar Prasad <rajkeshwar.pd@gmail.com>
2018-04-27 14:34:49 +0530

    Create broadcaster.ts

---
+++ markdown-docs/broadcaster.ts
@@ -0,0 +1,36 @@
+/*
+ * @Author: Rajkeshwar Prasad (rajkeshwar@dbs.com) 
+ * @Date: 2018-04-24 11:07:44 
+ * @Last Modified by:   Rajkeshwar Prasad 
+ * @Last Modified time: 2018-04-24 11:07:44 
+ */
+
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+
+//http://blog.lacolaco.net/post/event-broadcasting-in-angular-2/
+
+interface BroadcastEvent {
+  key: any;
+  data?: any;
+}
+
+export class Broadcaster {
+  private _eventBus: Subject<BroadcastEvent>;
+
+  constructor() {
+    this._eventBus = new Subject<BroadcastEvent>();
+  }
+
+  broadcast(key: any, data?: any) {
+    this._eventBus.next({key, data});
+  }
+
+  on<T>(key: any): Observable<T> {
+    return this._eventBus.asObservable()
+      .filter(event => event.key === key)
+      .map(event => <T>event.data);
+  }
+}
